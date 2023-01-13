64cdd44c306be10ad85113c42e2c6bbd705836cb
BogdanRakovchen <bogdan-dvzd.rzd@mail.ru>
2023-01-13 23:28:32 +1000

    add changes

---
+++ src/main/java/ru/skypro/homework/service/Ads/AdsInterface.java
@@ -0,0 +1,12 @@
+package ru.skypro.homework.service.Ads;
+
+
+
+public interface AdsInterface {
+    Ads getAds();
+    Ad addAds();
+    FullAd getFullAd(int id);
+    void removeAds(int id);
+    FullAd updateAds(int id);
+    String updateAdsImage(int id);
+}
