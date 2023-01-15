daf4f0de8f1d7e7b343c9f9dc388020e0acb488e
BogdanRakovchen <bogdan-dvzd.rzd@mail.ru>
2023-01-15 21:41:48 +1000

    изменены импорты в AuthController и подправлена зависимость в pom.xml

--- src/main/java/ru/skypro/homework/controller/AuthController.java
+++ src/main/java/ru/skypro/homework/controller/AuthController.java
@@ -8,12 +8,12 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import ru.skypro.homework.dto.LoginReq;
-import ru.skypro.homework.dto.RegisterReq;
-import ru.skypro.homework.dto.Role;
-import ru.skypro.homework.service.Autorization.AuthService;
+import ru.skypro.homework.model.user.LoginReq;
+import ru.skypro.homework.model.user.RegisterReq;
+import ru.skypro.homework.model.user.Role;
+import ru.skypro.homework.service.auth.AuthService;
 
-import static ru.skypro.homework.dto.Role.USER;
+import static ru.skypro.homework.model.user.Role.USER;
 
 @Slf4j
 @CrossOrigin(value = "http://localhost:3000")
