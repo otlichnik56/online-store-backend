399097b7a8f0681a1045fbcfe09faa35f5a0dcef
Hyp <otlichnik56@yandex.ru>
2023-02-06 23:00:21 +0500

    переделаны контроллеры, сервисы пользователей, объявлений и коментариев. переделан маппер, логика пересена в сервисы. удалены некоторые классы, а также переименованны. пока не проверял. нет разграничения доступа 06.02.2023 весь день

--- src/main/java/ru/skypro/homework/WebSecurityConfig.java
+++ src/main/java/ru/skypro/homework/WebSecurityConfig.java
@@ -20,7 +20,7 @@ public class WebSecurityConfig  {
             "/swagger-ui.html",
             "/v3/api-docs",
             "/webjars/**",
-            "/login", "/register"
+            "/login", "/register", "/ads"
     };
 
 
@@ -38,8 +38,7 @@ public class WebSecurityConfig  {
                 .authorizeHttpRequests((authz) ->
                         authz
                                 .mvcMatchers(AUTH_WHITELIST).permitAll()
-                                .mvcMatchers("/ads/**", "/users/**").authenticated()
-
+                                .mvcMatchers("/ads/**", "/users/**", "/image/**").authenticated()
                 )
                 .cors().and()
                 .httpBasic(withDefaults());
